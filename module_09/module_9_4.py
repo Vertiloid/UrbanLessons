176d037654711581b7f70b509f396fdd75bd882e
Vertiloid <647214036@rambler.ru>
2024-09-01 15:59:55 +0300

    Add files via upload

---
+++ module_09/module_9_4.py
@@ -0,0 +1,31 @@
+# Lambda-функция:
+first = 'Мама мыла раму'
+second = 'Рамена мало было'
+
+print(list(map(lambda f, s: f == s, first, second)))
+
+
+# Замыкание:
+def get_advanced_writer(file_name):
+    def write_everything(*data_set):
+        with open(file_name, 'a') as f:
+            for i in data_set:
+                f.write(str(i) + '\n')
+    return write_everything
+
+write = get_advanced_writer('example.txt')
+write('Это строчка', ['А', 'это', 'уже', 'число', 5, 'в', 'списке'])
+
+
+# Метод __call__:
+from random import choice
+class MysticBall:
+    def __init__(self, *args):
+        self.words = set(args)
+    def __call__(self):
+        return choice(list(self.words))
+
+first_ball = MysticBall('Да', 'Нет', 'Наверное')
+print(first_ball())
+print(first_ball())
+print(first_ball())
